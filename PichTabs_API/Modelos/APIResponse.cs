bd47ea4aa65dbfdfe3a57a6bba2e52a4023a1396
Thiago Emery <thiagofreireramirezemery@gmail.com>
2023-03-24 20:04:30 -0300

    ...blablabla

---
+++ PichTabs_API/Modelos/APIResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace PichTabs_API.Modelos
+{
+    public class APPPIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+
+        public List<string> Errors { get; set; }
+
+        public object Resultado { get; set; }
+    }
+}
